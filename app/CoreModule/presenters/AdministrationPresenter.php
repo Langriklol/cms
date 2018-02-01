13a570a25a20e7d1c467292cb7538d9a98bbcdff
Ondřej Langr <lango@doomsdaymachine.DOMA>
2018-02-01 12:34:05 +0100

    Administration abstract

---
+++ app/CoreModule/presenters/AdministrationPresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lango
+ * Date: 2/1/18
+ * Time: 12:29 PM
+ */
+
+namespace App\CoreModule\Presenters;
+
+
+class AdministrationPresenter
+{
+
+}
---
+++ app/CoreModule/presenters/ArticlePresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lango
+ * Date: 2/1/18
+ * Time: 12:39 AM
+ */
+
+namespace App\CoreModule\Presenters;
+
+
+class ArticlePresenter
+{
+
+}
